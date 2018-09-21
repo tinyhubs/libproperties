97aec668a050c6732a84a42a1987bb656af36070
libbylg <libbylg@126.com>
2018-09-21 08:14:52 +0800

    将C实现解析的代码基本完工

--- properties.c
+++ properties.c
@@ -12,12 +12,17 @@ struct p_cache_t
     char    cache[1024];
 };
 
-#define P_NEWLINE   (0x01)  //  \n  \0
-#define P_VISIBLE   (0x02)  //  
-#define P_SPLITER   (0x04)  //  : = \b
-#define P_ESCAPE    (0x08)  //  \   
-#define P_SPACE     (0x10)  //  \r  \t \b   注意不包含换行，因为换行需要特殊处理行号问题
-#define P_COMMENT   (0x20)  //  #   !
+enum
+{
+    P_NEWLINE   =   (0x01), //  \n  \0
+    P_SPLITER   =   (0x04), //  : = \b
+    P_ESCAPE    =   (0x08), //  \   
+    P_SPACE     =   (0x10), //  \r  \t \b   注意不包含换行，因为换行需要特殊处理行号问题
+    P_COMMENT   =   (0x20), //  #   !
+    P_EOS       =   (0x40), //  \0 
+};
+
+
 unsigned char cmap[256] = 
 {
 
@@ -28,8 +33,6 @@ struct p_cache_t
 {
     int     lino;
     char*   line;   //  行的起始位置
-    char*   pos;    //  当前已经识别到什么位置
-    char*   cache;  //  缓冲区大小
     char*   end;    //  cache 的结尾
 };
 
@@ -44,32 +47,17 @@ retry:
         pos++;
     }
 
-    //  如果不是换行标记，那么可以提前结束了，这里有利于提高性能
-    if (!(P_NEWLINE&cmap[*pos]))
-    {
-        return;
-    }
-
-    //  如果遇到的是真换行，那么需要重置行的起始位置和行号，然后重新扫描和跳过空白
-    while ('\n' == *pos)
-    {
-        pos++;
-        cache->pos = pos;
-        cache->lino++;
-        cache->line = pos;
-        goto    retry;
-    }
-
     //  如果遇到的是 \0 符号，那么我们可以认为可能遇到了缓冲区不够或者输入流截止的问题
     if ('\0' == *pos)
     {
-        //  如果是缓冲区不够
+        //  如果是缓冲区结尾标记
         if (pos == cache->end)
         {
-            cache->pos = pos
-            p_cache_read_more();    //  载入更多数据
-            pos = cache->pos;
-            if (pos == cache->end)  //  如果载入数据之后仍然不行
+            //  载入更多数据
+            pos = p_cache_read_more(cache, pos);
+            
+            //  如果载入数据之后，仍然是缓冲区结尾，说明已经数据全部处理完毕了
+            if (pos == cache->end)
             {
                 return;
             }
@@ -77,12 +65,9 @@ retry:
             //  如果缓冲区已经被新数据填充了，那么继续前面的处理
             goto    retry;
         }
-
-        //  如果输入字符串就是 \0，那么就当作正常符号处理，该符号统一在key的识别中会当作错误处理
-        return;
     }
 
-    //  程序不应该走到这里来
+    //  到这里可以确信已经 cache->pos 一定不是空白了  
     return;
 }
 
@@ -91,63 +76,293 @@ inline static void p_accept_comment(struct p_cache_t* cache)
 {
     register char* pos = cache->pos;
 
-    //  只要不是换行或者cache结尾符号
-    while (!(P_NEWLINE&cmap[*pos]))
+retry:
+
+    //  注释可能会因为输入流结束或者读到文件尾而结束
+    while (!((P_NEWLINE|P_EOS)&cmap[*(++pos)]))
     {
-        pos++;
     }
 
-    //  如果遇到真换行,这里不处理，而是交给换行处理机制处理
-    if ('\n' == *pos)
+    //  如果遇到换行,那么注释就结束了
+    if (P_NEWLINE&cmp[*pos])
     {
         cache->pos = pos;
+        return;
+    }
+
+    if (P_EOS&cmp[*pos])
+    {
+        //  载入更多数据
+        pos = p_cache_read_more(cache, pos);
+
+        //  如果载入数据之后，仍然是缓冲区结尾，说明已经数据全部处理完毕了
+        if (pos == cache->end)
+        {
+            return;
+        }
+
+        //  如果缓冲区已经被新数据填充了，那么继续前面的处理
+        goto    retry;
     }
 
     return;
 }
 
 
-char* p_accept_key(char* s)
+void p_accept_key(struct p_cache_t* cache)
 {
     char* pos = s;
 
 retry:
-    //  跳过所有的空白
-    while (P_SPACE&cmap[*pos])
+    
+    while (!(P_SPACE|P_NEWLINE|P_ESCAPE|P_EOS|P_SPLITER)&cmap[*pos])
     {
         pos++;
         continue;
     }
 
-    //  如果第一个非空白字符是注释（将这两类折合在一起计算，提高性能）
-    if ((P_COMMENT|P_EOS)&cmap[*pos])
+    switch (cmp[*pos])
     {
-        if (P_COMMENT&cmap[*pos])
+    case P_SPACE:
+    case P_SPLITER:
+        //  生成key
+        cache->pos = pos;
+        return;
+    case P_NEWLINE:
+        //  生成key，并向props添加key
+        cache->pos = pos;
+        return;
+    case P_ESCAPE:
+        p_accept_escape(cache);
+        goto    retry;
+    case P_EOS:
+        //  如果实际是缓冲区结束标志
+        if (pos == cache->end)
         {
-            //  跳过空白区域直到\n符号
-            p_accept_comment()
+            //  TODO 这里需要将字符串存到cache里面去
+            p_cache_read_more(cache);
+            if (pos == cache->end)
+            {
+                //  生成key，并向props添加key
+                cache->pos = pos;
+                return;
+            }
+
             goto    retry;
         }
 
-        
-        //  检查一下是否真的到\0了，还是说只是缓冲区慢了
+        //  真遇到 \0 实际已经无法继续下去了
+        goto    retry;
+    default:
+        //  不应该走到这里来
+        ASSERT(0)
+    }
+
+    //  不应该走到这里来
+    ASSERT(0)
+}
+
+inline static void p_accept_unicode_escape(struct p_cache_t* cache)
+{
+    //  \uxxxx
+    cache->pos += 2;
+
+    //  如果提前结束
+    if ((cache->pos + 4) > cache->end)
+    {
+        p_cache_read_more(cache, cache->pos, 4);    //  确保至少有4个字符
+        if ((cache->pos + 4) > cache->end)
+        {
+            //  要报错吗？？？
+            return;
+        }
+    }
+
+    //  对数据进行格式转换
+}
+
+inline static void p_accept_escape(struct p_cache_t* cache)
+{
+    cache->pos++;
+
+retry:
+
+    switch (*pos)
+    {
+    case '\0':
+        //  如果实际是缓冲区结束标志
+        if (pos == cache->end)
+        {
+            //  TODO    此时要将字符串存起来
+            pos = p_cache_read_more(cache, pos);
+            if (pos == cache->end)
+            {
+                return;
+            }
+
+            goto    retry;
+        }
+
+        //  真遇到 \0 实际已经无法继续下去了
+        goto    retry;
+    case 'n':
+        //  TODO \n
+        return;
+    case 't':
+        //  TODO \t
+        return;
+    case 'r':
+        //  TODO \t
+        return;
+    case 'u':
+        register char* pos = s;
+        //unicode转义
+        cache->pos = pos;
+        p_accept_unicode_escape(cache);
+        return;
+    default:
+        // *pos
+        return;
+    }
+}
+
+
+
+
+inline static void p_accept_value(struct p_cache_t* cache)
+{
+    register char* pos = s;
+
+retry:
+
+    while (!(P_NEWLINE|P_ESCAPE|P_EOS)&cmap[*pos])
+    {
+        pos++;
+        continue;
+    }
+
+    switch (cmp[*pos])
+    {
+    case P_NEWLINE:
+        //  生成key，并向props添加key
+        cache->pos = pos;
+        return;
+    case P_ESCAPE:
+        p_accept_escape(cache);
+        goto    retry;
+    case P_EOS:
+        //  如果实际是缓冲区结束标志
+        if (pos == cache->end)
+        {
+            //  TODO    此时要将字符串存起来
+            pos = p_cache_read_more(cache, pos);
+            if (pos == cache->end)
+            {
+                //  生成key，并向props添加key
+                cache->pos = pos;
+                return;
+            }
+
+            goto    retry;
+        }
+
+        //  真遇到 \0 实际已经无法继续下去了
+        goto    retry;
+    default:
+        //  不应该走到这里来
+        ASSERT(0)
+    }
+
+    //  不应该走到这里来
+    ASSERT(0)
+}
+
+void p_accept_spliter(struct p_cache_t* cache)
+{
+    register char* pos = cache->pos;
+
+retry:
+
+    while (!((P_SPACE|P_SPLITER|P_EOS|P_NEWLINE)&cmp[*pos]))
+    {
+        pos++;
+    }
+    cache->pos = pos;
+
+    if (P_EOS&cmp[*pos])
+    {
+        //  如果实际是缓冲区结束标志
+        if (pos == cache->end)
+        {
+            p_cache_read_more(cache);
+            if (pos == cache->end)
+            {
+                return;
+            }
+
+            goto    retry;
+        }
+
+        //  真遇到 \0 实际已经无法继续下去了
+        return;
+    }
+
+    if ((P_SPACE|P_SPLITER)&cmp[pos])
+    {
+        p_skip_space(cache);
+        return;
+    }
+
+    if (P_NEWLINE&cmp[pos])
+    {
+        return;
+    }
+}
+
+int properties_parse(struct p_cache_t* cache)
+{
+    register char* pos = cache->pos;
+
+retry:
+
+    p_skip_space(cache);
+    pos = cache->pos;
+
+    //  如果是注释行，跳过
+    if (P_COMMENT&cmp[*pos])
+    {
+        p_accept_comment(cache);
         goto    retry;
     }
 
-    register char c = pos[0];
-    
-    read_key();
+    //  如果是空行，修改行号和行起始
+    if (P_NEWLINE&cmp[*pos])
+    {
+        cache->pos++;
+        cache->line = cache->pos;
+        cache->lino++;
+        goto    retry;
+    }
 
-    
+    //  如果遇到\0符号，如果是输入流结尾，那么识别结束
+    if (P_EOS&cmp[*pos])
+    {
+        //  如果是行尾，说明已经无法再载入更多数据，识别结束
+        if (pos == cache->end)
+        {
+            return 0;
+        }
+    }
+
+    p_accept_key(cache);
 }
 
-struct properties_t*    properties_new          ()
-{
-
-}
 
 
-void                    properties_del          (struct properties_t* p)
+
+
+
+struct properties_t*    properties_new         void                    properties_del          (struct properties_t* p)
 {
 
 }
