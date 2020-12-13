bb2b898dc88800b3a98575d7301000da0166c483
libbylg <libbylg@126.com>
2020-12-13 23:49:22 +0800

    add build script

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 
 SELFDIR=$(realpath $(dirname "$0"))
+PROJECT_NAME=$(cat    "${SELFDIR}/NAME")
+PROJECT_VERSION=$(cat "${SELFDIR}/VERSION")
+PROJECT_HARDWARE=$(uname -m)
+PROJECT_SYSTEM=$(uname -s)
 
 function do_help()
 {
@@ -11,6 +15,7 @@ function do_help()
     echo    "   build.sh test"
     echo    "   build.sh format"
     echo    "   build.sh encode"
+    echo    "   build.sh clean"
     echo    "   build.sh help|-h|--help"
     return  0
 }
@@ -19,12 +24,48 @@ function do_help()
 #   $2  mode(debug or release)
 function do_compile()
 {
-    echo    "TODO do_compile"
+    rm -rf "${SELFDIR}/build"
+    
+    mkdir -p "${SELFDIR}/build"
+    cd "${SELFDIR}/build" && \
+    cmake ../             && \
+    make  clean           && \
+    make
+    RESULT=$?
+    if [[ ${RESULT} -ne 0 ]]; then
+        echo   "Error: Compile project '${PROJECT_NAME}' with mode '$2' failed(${RESULT})"
+        return 1
+    fi
+    
+    echo   "Compile project '${PROJECT_NAME}' with mode '$2' success"
+    return 0
 }
 
 function do_package()
 {
-    echo    "TODO do_package"
+    rm -rf      "${SELFDIR}"/*.tar.gz
+    rm -rf      "${SELFDIR}"/*.tar
+    rm -rf      "${SELFDIR}/pack"
+
+    mkdir   -p  "${SELFDIR}/pack/include"
+    mkdir   -p  "${SELFDIR}/pack/lib"
+
+    local package_name="${PROJECT_NAME}-${PROJECT_SYSTEM}-${PROJECT_HARDWARE}-${PROJECT_VERSION}"
+
+    cd          "${SELFDIR}/pack"                                       &&  \
+    cp -rf      "${SELFDIR}/lib"/*          "${SELFDIR}/pack/lib/"      &&  \
+    cp -rf      "${SELFDIR}/properties.h"   "${SELFDIR}/pack/include/"  &&  \
+    tar -cvzf   "${package_name}.tar.gz"    *                           &&  \
+    mv          "${package_name}.tar.gz"    "${SELFDIR}"
+    RESULT=$?
+    if [[ ${RESULT} -ne 0 ]]; then
+        echo    "Error: Create package failed(${RESULT})"
+        return  2
+    fi
+
+    rm -rf      "${SELFDIR}/pack"
+
+    return  0
 }
 
 function do_test()
@@ -42,18 +83,40 @@ function do_encode()
     echo    "TODO do_encode"
 }
 
+function do_clean()
+{
+    echo    "TODO do_clean"
+}
+
 function main()
 {
-    if   [[ "$1" == "debug" ]] || [[ "$1" == "release" ]]; then
+    if   [[ "$1" == "" ]]; then
+        do_compile  "compile"   "debug"
+        return      "$?"
+    elif [[ "$1" == "debug" ]] || [[ "$1" == "release" ]]; then
         do_compile  "compile"   "$@"
+        return      "$?"
     elif [[ "$1" == "compile" ]]; then
         do_compile  "$@"
+        return      "$?"
     elif [[ "$1" == "test" ]]; then
-        do_test  "$@"
+        do_test     "$@"
+        return      "$?"
     elif [[ "$1" == "package" ]]; then
         do_package  "$@"
+        return      "$?"
     elif [[ "$1" == "help" ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
-        do_package  "$@"
+        do_help     "$@"
+        return      "$?"
+    elif [[ "$1" == "clean" ]]; then
+        do_clean    "$@"
+        return      "$?"
+    elif [[ "$1" == "encode" ]]; then
+        do_encode    "$@"
+        return      "$?"
+    elif [[ "$1" == "format" ]]; then
+        do_format   "$@"
+        return      "$?"
     else
         echo    "Unsupported command '$1', type -h for help"
         return  1
