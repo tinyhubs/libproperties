2a243ccd1a5091c829349c4a493e1e615a191d65
libbylg <libbylg@126.com>
2020-12-20 18:13:53 +0800

    remove java deps

--- properties_test.c
+++ properties_test.c
@@ -6,35 +6,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "properties.h"
 
+#if !defined(ASSERT)
+#define ASSERT(x) assert(x)
+#endif
+
 int     test_handler(void* context, char* key, int key_len, char* val, int val_len)
 {
-    printf("[%s]=[%s]\n", key, val);
+    fprintf((FILE*)context, "[%s]=[%s]\n", key, val);
     return 0;
 }
 
+int generate_output(const char* fileNameC, const char* resultFileC)
+{
+    FILE* file = fopen(fileNameC, "r");
+    if (NULL == file) {
+        return 2;
+    }
+
+    FILE* resultC = fopen(resultFileC, "w+");
+    if (NULL == resultC) {
+        return 3;
+    }
+
+    int ret = properties_parse(file, properties_source_file_read, resultC, test_handler);
+    fclose(file);
+
+    return ret;
+}
+
+int is_file_equal(const char* resultFileC, const char* resultFileJ)
+{
+    FILE* fileC = fopen(resultFileC, "r");
+    FILE* fileJ = fopen(resultFileJ, "r");
+    ASSERT(NULL != fileC);
+    ASSERT(NULL != fileJ);
+
+    size_t bufSizeMax = 1 * 1024 * 1024;
+    char* bufC = (char*)malloc(bufSizeMax);
+    char* bufJ = (char*)malloc(bufSizeMax);
+    ASSERT(NULL != bufC);
+    ASSERT(NULL != bufJ);
+
+    while (feof(fileC) || feof(fileJ)) {
+        char* lineC = fgets(bufC, sizeof(bufC), fileC);
+        char* lineJ = fgets(bufJ, sizeof(bufJ), fileJ);
+        ASSERT(NULL != lineC);
+        ASSERT(NULL != lineJ);
+        printf("C[%s]\n", lineC);
+        printf("L[%s]\n", lineJ);
+        int lineResult = strncmp(lineC, lineJ, bufSizeMax);
+        ASSERT(lineResult == 0);
+    }
+
+    ASSERT(feof(fileC));
+    ASSERT(feof(fileJ));
+
+    free(bufC);
+    free(bufJ);
+
+    return 0;
+}
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
-    {
-        printf("Missing parameters: properties_test <FILE>");
+    if (argc < 4) {
+        printf("Missing parameters: properties_test <FILE-C>  <FILE-C-OUTPUT>  <FILE-J-EXPECT>\n");
         return  1;
     }
 
-    FILE* file = fopen(argv[1], "r");
-    if (NULL == file)
-    {
-        return  2;
-    }
+    int ret = generate_output(argv[1], argv[2]);
+    ASSERT(0 == ret);
+    ASSERT(is_file_equal(argv[2], argv[3]));
 
-    int ret = properties_parse(file, properties_source_file_read, NULL, test_handler);
-    fclose(file);
-    
-    //getchar();
-    return ret;
+    return 0;
 }
 
 
